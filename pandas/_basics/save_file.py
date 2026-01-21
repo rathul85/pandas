fe73fa1eb5644a099e9017c244404db45f0713a8
rathul85 <rathulhasansaion5@gmail.com>
2026-01-21 22:42:30 +0600

    added save file example

---
+++ pandas/_basics/save_file.py
@@ -0,0 +1,15 @@
+'''
+topic:save file
+'''
+import pandas as pd
+data={
+    "name":["rathul","sakib","neymar"],
+    "age":[10,20,30],
+    "city":["frankfurt","america","brazil"]
+}
+df=pd.DataFrame(data)
+print(df)
+df.to_csv("output.csv",index=False)
+#df.to_excel("output.xlsx")
+#df.to_json("output.json")
+
